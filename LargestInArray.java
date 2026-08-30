b0316c5f253d6e0541278929d591b1eb4b60f2ef
Suraj Aglave <aglavesuraj96@gmail.com>
2026-08-30 17:54:41 +0530

    Create LargestInArray.java

---
+++ LargestInArray.java
@@ -0,0 +1,12 @@
+class LargestInArray {
+    public static int largest(int[] arr) {
+        // code here
+        int max = arr[0];
+        for (int i : arr) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
